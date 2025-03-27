9b02995eaaae54f62458ffa9a4a8fd5c66e23fa6
shouqak <shouqbis@gmail.com>
2025-03-28 02:38:04 +0300

    name

--- eid_mubarak.js
+++ eid_mubarak.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["EID", "MUBARAK!","" ,"","SHOUQ"],
+    strings: ["EID", "MUBARAK!","" ,"","SHOUQ ALKANHAL"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
